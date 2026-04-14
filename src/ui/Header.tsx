09bc3b99388ad281f63bc6a59b2ea2176eef37e4
ajzmbhc <ajzelaya@banhcafe.hn>
2026-04-14 15:14:14 -0600

    fix: change location of Logos

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,7 +1,7 @@
-import minLogo from "../assets/logo.png";
-import logo from "../assets/logo.webp";
 import { useMarkContext } from "../contexts/useMarkContext";
 import User from "../features/user/User";
+import minLogo from "/logo.png";
+import logo from "/logo.webp";
 
 export default function Header() {
   const { user } = useMarkContext();
