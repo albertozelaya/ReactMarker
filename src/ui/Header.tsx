482b90b9e820b3abb72d2861472f2aac161ab1c7
ajzmbhc <ajzelaya@banhcafe.hn>
2026-04-14 15:37:12 -0600

    fix: change logo type as svg component

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,7 +1,6 @@
+import Logo from "../components/Logo";
 import { useMarkContext } from "../contexts/useMarkContext";
 import User from "../features/user/User";
-import minLogo from "/logo.png";
-import logo from "/logo.webp";
 
 export default function Header() {
   const { user } = useMarkContext();
@@ -10,16 +9,9 @@ export default function Header() {
     <header>
       {user && <User />}
 
-      <picture>
-        <source srcSet={logo} type="image/webp" />
-        <source srcSet={minLogo} type="image/png" />
-
-        <img
-          src={logo}
-          alt="banhcafe-logo"
-          className="3xl:w-13 mx-auto my-0 mb-4 w-8 lg:w-9 xl:mb-4 xl:w-10"
-        />
-      </picture>
+      <div className="3xl:w-13 mx-auto my-0 mb-4 w-8 lg:w-9 xl:mb-4 xl:w-10">
+        <Logo />
+      </div>
 
       <h1 className="3xl:text-6xl 3xl:tracking-tight text-center text-5xl font-bold tracking-tight text-gray-800 sm:text-[2.75rem] md:tracking-wide lg:text-5xl lg:tracking-tight xl:tracking-normal">
         Registro de Asistencia
